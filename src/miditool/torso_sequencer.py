0ccfd1d685242c42edf3f87ec4b6cea90cc2a6d5
Rusty Brooks <rbrooks@alienvault.com>
2021-02-12 22:29:19 -0600

    Got a basic euclidean synth working

--- src/miditool/torso_sequencer.py
+++ src/miditool/torso_sequencer.py
@@ -1,3 +1,4 @@
+import math
 import logging
 import threading
 import time
@@ -14,16 +15,18 @@ log = logging.getLogger(__name__)
 
 
 class TorsoTrack:
+    accent_curves = [
+        [x/100. for x in [70, 20, 70, 20, 80, 90, 20, 60, 20, 60, 20, 60, 20, 90, 80, 20, 70]],
+    ]
+
     def __init__(
         self, channel=0, notes=None, steps=16, pulses=1, pitch=0, rotate=0, manual_steps=None,
         accent=0, accent_curve=0, sustain=0.5, division=0, velocity=64, timing=0, swing=0, repeats=0,
-        offset=0, time=0,
+        offset=0, time=0, bpm=200,
     ):
         self.channel = channel
         self.notes = notes or []
         self.manual_steps = manual_steps or []
-        self.steps = steps
-        self.pulses = pulses
         self.pitch = pitch
         self.rotate = rotate
         self.division = division  # ??
@@ -37,41 +40,85 @@ class TorsoTrack:
         self.offset = offset
         self.time = time
 
+        # require re-sequencing:
+        self.steps = steps
+        self.pulses = pulses
+
+        self.sequence = []
+
+        # some internal params
+        self._sequence_start = None
+        self._bpm = None
+        self._beat = None
+
+    def set_bpm(self, value):
+        self._bpm = value
+        self._beat = 60. / value
+
     def generate(self):
         interval = self.steps / self.pulses
-        events = []
+
+        self.sequence = [None]*self.steps
+
         for i in range(self.pulses):
             spot = int(i*interval)
             note = self.notes[spot % len(self.notes)]
+            self.sequence[spot] = note
+
+    def fill_lookahead(self, start, end):
+        # print(start, end, (start - self._sequence_start)/self._beat, (end - self._sequence_start)/self._beat)
+        first_step = math.ceil((start - self._sequence_start)/self._beat)
+        last_step = math.floor((end - self._sequence_start)/self._beat)
+
+        if last_step < first_step:
+            return []
+
+        events = []
+        for step in range(first_step, last_step+1):
+            note = self.sequence[step % self.steps]
+            if not note:
+                continue
+
             events.extend([
-                (spot + self.offset, (NOTE_ON+self.channel, note, self.velocity)),
-                (spot + self.offset+self.sustain, (NOTE_OFF+self.channel, note, 0)),
+                MidiEvent(
+                    (step + self.offset)*self._beat,
+                    (NOTE_ON+self.channel, note, self.velocity)
+                ),
+                MidiEvent(
+                    (step + self.offset+self.sustain)*self._beat,
+                    (NOTE_OFF+self.channel, note, 0)
+                ),
             ])
 
         return events
 
-    def fill_lookahead(self, start, end):
-        pass
-
 
 class TorsoSequencer(threading.Thread):
-    def __init__(self, midiout, interval=0.002, lookahead=1):
+    def __init__(self, midiout, interval=0.002, lookahead=.02, bpm=200):
         super().__init__()
         self.midiout = midiout
         self.interval = interval
         self.lookahead = lookahead
         self.start_time = None
-        self.next_lookahead = None
         self.last_lookahead = None
         self.pending = []
 
         self.tracks = {}
+        self.bpm = bpm
 
         self._stopped = threading.Event()
         self._finished = threading.Event()
 
+    def set_bpm(self, value):
+        for t in self.tracks.values():
+            t.set_bpm(value)
+
+        self.bpm = value
+
     def add_track(self, track_name, track):
         self.tracks[track_name] = track
+        track.set_bpm(self.bpm)
+        track.generate()
 
     def stop(self, timeout=5):
         """Set thread stop event, causing it to exit its mainloop."""
@@ -84,9 +131,8 @@ class TorsoSequencer(threading.Thread):
 
     def fill_lookahead(self):
         next_lookahead = self.last_lookahead + self.lookahead
-        print(time.time(), self.last_lookahead, next_lookahead)
         for v in self.tracks.values():
-            new = v.fill_lookahead(self.last_lookahead, self.next_lookahead)
+            new = v.fill_lookahead(self.last_lookahead, next_lookahead)
             if new:
                 for n in new:
                     heappush(self.pending, n)
@@ -98,9 +144,12 @@ class TorsoSequencer(threading.Thread):
         try:
             self.start_time = time.time()
             self.last_lookahead = self.start_time
+            for t in self.tracks.values():
+                t._sequence_start = self.start_time
+
             self.fill_lookahead()
             self.fill_lookahead()
-            print("...", time.time(), self.last_lookahead, self.next_lookahead)
+            # return
 
             while not self._stopped.is_set():
                 t1 = time.time()
@@ -118,7 +167,7 @@ class TorsoSequencer(threading.Thread):
 
                 if t1 >= self.last_lookahead:
                     self.fill_lookahead()
-                    return
+                    continue
 
                 steps += 1
                 left = (self.interval*steps) - (time.time() - self.start_time)
--- src/scripts/torso_test.py
+++ src/scripts/torso_test.py
@@ -22,33 +22,52 @@ midiout, port = open_midiport(
 )
 time.sleep(1)
 
-tt = torso_sequencer.TorsoTrack(
+tt1 = torso_sequencer.TorsoTrack(
     notes=[notes.note_to_number(['D#', 3])],
     pulses=9,
 )
+tt2 = torso_sequencer.TorsoTrack(
+    notes=[notes.note_to_number(['C#', 3])],
+    pulses=4,
+)
+tt3 = torso_sequencer.TorsoTrack(
+    notes=[notes.note_to_number(['D#', 2])],
+    pulses=5,
+)
 
-snotes = tt.generate()
-print(snotes)
+if 0:
 
-seq = Sequencer(midiout, bpm=320, ppqn=240)
-for el in snotes:
-    tick, event = el
-    seq.add(event=event, tick=(tick*seq.ppqn))
+    t1 = time.time()
+    snotes = tt.generate()
+    print(1000*(time.time()-t1), snotes)
 
-try:
-    seq.start()
-    seq.join()
-finally:
-    seq.stop()
-    midiout.close_port()
-    del midiout
+    seq = Sequencer(midiout, bpm=320, ppqn=240)
+    for el in snotes:
+        tick, event = el
+        seq.add(event=event, tick=(tick*seq.ppqn))
 
-# t = torso_sequencer.TorsoSequencer(midiout=midiout)
-#
-# try:
-#     t.start()
-#     t.join()
-# finally:
-#     t.stop()
-#     midiout.close_port()
-#     del midiout
+    try:
+        seq.start()
+        seq.join()
+    finally:
+        seq.stop()
+        midiout.close_port()
+        del midiout
+else:
+    t = torso_sequencer.TorsoSequencer(
+        midiout=midiout,
+        lookahead=0.02,
+        bpm=400,
+    )
+
+    t.add_track(track_name='hihat', track=tt1)
+    t.add_track(track_name='snare', track=tt2)
+    t.add_track(track_name='clap', track=tt3)
+
+    try:
+        t.start()
+        t.join()
+    finally:
+        t.stop()
+        midiout.close_port()
+        del midiout
