6c0c7af4f22a7617f6b20014034c73d2299fc5bd
Rusty Brooks <rbrooks@alienvault.com>
2021-11-28 20:09:42 -0600

    Move midi interface stuff into index.ts

--- src/ui/src/index.tsx
+++ src/ui/src/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import { withStore, useGetAndSet } from 'react-context-hook'
 
 // import { Link } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles';
@@ -10,8 +11,20 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 // import { withRouter } from 'react-router'
 
+import MidiMessage from "./MidiMessage";
 
-import App from './App'
+interface MidiCallback {
+  (message: MidiMessage): null
+}
+
+interface MidiInputs {
+  [id: number]: any
+}
+
+interface CallbackMap {
+  [id: number]: {[id: number]: MidiCallback}
+}
+
 
 const styles = {
   root: {
@@ -32,10 +45,58 @@ const styles = {
 };
 
 const NavBarX = ({classes} : any) => {
+  let midiInputs : MidiInputs;
+  let setMidiInputs : any;
+  let callbackMap : CallbackMap = {}
+
+  let access_callback = (access : any) => {
+    let _midiInputs : MidiInputs = {}
+
+    for (let input of access.inputs.values()) {
+      input.onmidimessage = callback;
+      callbackMap[input.id] = {}
+      midiInputs[input.id] = input
+    }
+
+    access.onstatechange = (e : any) => {
+      setMidiInputs({...midiInputs, [e.port.id]: e.port})
+    }
+
+    setMidiInputs(_midiInputs);
+  }
+
+  let init = () => {
+    console.log("init pre")
+    navigator.requestMIDIAccess().then(access_callback)
+    console.log("init post")
+  }
+
+  let listen = (midi_id : number, listen_id : number, callback : MidiCallback) => {
+    // console.log('listen', midi_id, callback)
+    callbackMap[midi_id][listen_id] = callback
+  }
+
+  let callback = (m : any) => {
+    const message = new MidiMessage(m)
+    // console.log(m)
+    const midi_id = m.target.id
+    // console.log("midi cb", midi_id, callbackMap[midi_id])
+    Object.values(callbackMap[midi_id]).forEach((cb) => {
+       cb(message)
+    })
+  }
+
+  [midiInputs, setMidiInputs] = useGetAndSet('midiInputs')
+
+  React.useEffect(() => {
+    init()
+  }, [])
+
+
     return (
         <div className={classes.root}>
             <AppBar position="static">
-                <Tabs value={1}>
+                <Tabs value={0}>
                     <Tab
                         key='Home'
                         label='home'
@@ -47,7 +108,13 @@ const NavBarX = ({classes} : any) => {
     )
 }
 
-const NavBar = withStyles(styles)(NavBarX)
+const initialValue = {
+  'midiCallbackMap': {},
+  'midiInputs': {},
+}
+
+const NavBar = withStore(withStyles(styles)(NavBarX), initialValue)
+
 
 ReactDOM.render(
     <BrowserRouter>
