beb0cf1c9f91a27e3ba629bfa9aaae5fe810e39a
Rusty Brooks <me@rustybrooks.com>
2022-02-13 22:03:00 -0600

    tweaks

--- src/ui/src/components/sequencers/Torso.tsx
+++ src/ui/src/components/sequencers/Torso.tsx
@@ -9,7 +9,7 @@ function Knob({ k }: { k: any }) {
   return (
     <div style={{ textAlign: 'center', padding: '.5rem' }}>
       <svg width="20mm" height="20mm" viewBox="0 0 20 20" version="1.1" transform="rotate(45)">
-        <circle fill="#ff0000" fillRule="evenodd" stroke="#a10000" strokeWidth=".5" strokeOpacity="1" cx="10" cy="10" r="9" />
+        <circle fill="#aa6666" fillRule="evenodd" stroke="#a10000" strokeWidth=".5" strokeOpacity="1" cx="10" cy="10" r="9" />
         <circle fill="#000000" cx="10" cy="4" r="2" />
       </svg>{' '}
       <div>{k.label}</div>
@@ -23,8 +23,24 @@ function Button({ b }: { b: any }) {
     return <div style={{ width: '5rem', height: '5rem' }} />;
   }
   return (
-    <div style={{ textAlign: 'center', verticalAlign: 'top' }}>
-      <div style={{ width: '5rem', height: '5rem', backgroundColor: '#66a' }} />
+    <div style={{ textAlign: 'center', verticalAlign: 'top', fontSize: '1rem' }}>
+      <div style={{ display: 'flex', textAlign: 'left' }}>
+        <div style={{ width: '5rem', height: '5rem', backgroundColor: '#66a' }} />
+        <div
+          style={{
+            transform: 'rotate(-90deg) translate(0, 1.8rem)',
+            position: 'relative',
+            float: 'right',
+            height: '5rem',
+            width: '1rem',
+            textAlign: 'left',
+            verticalAlign: 'top',
+            padding: 'auto',
+          }}
+        >
+          {b[1]}&nbsp;
+        </div>
+      </div>
       <div>{b[0]}&nbsp;</div>
     </div>
   );
