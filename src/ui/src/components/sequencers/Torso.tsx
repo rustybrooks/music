a06c7e8e72c0f26462b47e6376652110360f2fcb
Rusty Brooks <me@rustybrooks.com>
2022-02-19 11:36:51 -0600

    make stuff smaller

--- src/ui/src/components/sequencers/Torso.tsx
+++ src/ui/src/components/sequencers/Torso.tsx
@@ -7,41 +7,42 @@ import { useGetAndSet } from 'react-context-hook';
 
 function Knob({ k }: { k: any }) {
   return (
-    <div style={{ textAlign: 'center', padding: '.5rem' }}>
-      <svg width="20mm" height="20mm" viewBox="0 0 20 20" version="1.1" transform="rotate(45)">
+    <div style={{ textAlign: 'center', padding: '.4rem' }}>
+      <svg width="4rem" height="4rem" viewBox="0 0 20 20" version="1.1" transform="rotate(45)">
         <circle fill="#aa6666" fillRule="evenodd" stroke="#a10000" strokeWidth=".5" strokeOpacity="1" cx="10" cy="10" r="9" />
         <circle fill="#000000" cx="10" cy="4" r="2" />
       </svg>{' '}
-      <div>{k.label}</div>
-      <div>{k.alt_label}&nbsp;</div>
+      <div style={{ fontSize: '.8rem' }}>{k.label}</div>
+      <div style={{ fontSize: '.8rem' }}>{k.alt_label}&nbsp;</div>
     </div>
   );
 }
 
 function Button({ b }: { b: any }) {
   if (b === null) {
-    return <div style={{ width: '5rem', height: '5rem' }} />;
+    return <div style={{ width: '4rem', height: '4rem' }} />;
   }
   return (
     <div style={{ textAlign: 'center', verticalAlign: 'top', fontSize: '1rem' }}>
       <div style={{ display: 'flex', textAlign: 'left' }}>
-        <div style={{ width: '5rem', height: '5rem', backgroundColor: '#999' }} />
+        <div style={{ width: '3rem', height: '3rem', backgroundColor: '#999' }} />
         <div
           style={{
-            transform: 'rotate(-90deg) translate(0, 1.8rem)',
+            transform: 'rotate(-90deg) translate(0, 1.1rem)',
             position: 'relative',
             float: 'right',
-            height: '5rem',
-            width: '1rem',
+            height: '3rem',
+            width: '.8rem',
             textAlign: 'left',
             verticalAlign: 'top',
             padding: 'auto',
+            fontSize: '.6rem',
           }}
         >
           {b[1]}&nbsp;
         </div>
       </div>
-      <div>{b[0]}&nbsp;</div>
+      <div style={{ fontSize: '.8rem' }}>{b[0]}&nbsp;</div>
     </div>
   );
 }
